a9be546092bbb2510917b90c498af533abbf2c06
shudhanshuinnostax <shudhanshu.shree@innostax.com>
2025-02-11 11:42:25 +0530

    add: new todo function added

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState } from 'react';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [inputData, setInputData] = useState('');
+  const [todoItem, setTodoItem] = useState([]);
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    if (inputData !== '') {
+      setTodoItem([...todoItem, inputData]);
+      setInputData('')
+    }
+
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='app'>
+      <div className='heading'><h1>TODO CRUD</h1></div>
+      <div className='todo-form'>
+        <form onSubmit={handleFormSubmit}>
+          <input type="text" value={inputData} onChange={(e) => setInputData(e.target.value)} />
+          <button type='submit'>Add Todo</button>
+        </form>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className='todo-list'>
+        {
+          todoItem.map((item,index) => (
+            <h2 key={index}>{item}</h2>
+          ))
+        }
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
-export default App
+export default App;
